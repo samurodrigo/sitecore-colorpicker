5b996e1446e40d8ef3d806cd82873d87112aa4dd
Samuel Ferreira <samuel.ferreira@valtech.com>
2024-09-26 17:10:03 -0300

    Add project files.

---
+++ Sitecore.CustomFields.ColorBuddy.Project/ColorBuddy.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.UI;
+
+namespace Sitecore.CustomFields.ColorBuddy.Project
+{
+    public class ColorBuddy: Sitecore.Web.UI.HtmlControls.Edit
+    {
+        protected override void DoRender(HtmlTextWriter output)
+        {
+            output.Write("<input type='color' id='{0}' name='{1}' value='{2}' /><span style='color:{2}; margin-left:10px'>{2}</span>", this.ID, this.Name, this.Value);
+        }
+    }
+}
